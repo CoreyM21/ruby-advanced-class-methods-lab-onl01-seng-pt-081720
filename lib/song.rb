6b492826c70a5b0453ce0a4e7a5468be776e2160
Corey Martin <coreyjmartin2@gmail.com>
2020-09-11 02:14:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,32 @@
 class Song
   attr_accessor :name, :artist_name
+ 
   @@all = []
+  
+  def self.create
+    song = self.new
+    song.save
+    song 
+  end
+  
+  def self.new_by_name(name)
+    song = self.new 
+    song.name = name 
+    song 
+  end
+  
+  def self.create_by_name(name)
+    song = self.new 
+    song.name = name
+    song.save
+    song
+  end
+  
+  def self.find_by_name(name)
+    @@all.find do |song|
+      song.name == name 
+    end
+  end
 
   def self.all
     @@all
@@ -9,5 +35,24 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) == nil 
+      return self.create_by_name(name)
+    else 
+      return self.find_by_name(name)
+   end
+  end
+  
+  def self.alphabetical
+    array = self.all
+    array.sort do |song_element_1, song_element_2|
+      song_element_1.name <=> song_element_2.name
+    end
+  end
+  
+  def self.destroy_all
+    @@all.clear
+  end
+  
 end
